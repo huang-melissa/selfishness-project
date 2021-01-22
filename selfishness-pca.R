dda75efe3793fdcc21797246043ddc6be9969905
huang-melissa <mhuang.teaching@gmail.com>
2021-01-21 22:30:43 -0500

    added exploratory factor analysis

--- selfishness-pca.R
+++ selfishness-pca.R
@@ -1,3 +1,8 @@
+library(stats)
+library(dplyr)
+library(psych)
+library(nFactors)
+
 #conduct PCA on SQ
 
 selfish_latent_all <- selfish_data[,c(436:459)] #full sq
@@ -12,17 +17,18 @@ head(selfish_latent_all) #take a look at data to see if selection successful
 
 #principle component analysis
 
-fit <- princomp(na.omit(selfish_latent_all), cor=FALSE) #use covariance matrix since vars same scale
-summary(fit)
-loadings(fit)
-plot(fit, type="lines")
-fit$scores
-biplot(fit)
+sq_all.pca <- princomp(na.omit(selfish_latent_all), cor=FALSE) #use covariance matrix since vars same scale
+summary(sq_all.pca)
+loadings(sq_all.pca)
+plot(sq_all.pca) #bar
+plot(sq_all.pca, type="lines") #scree
+sq_all.pca$scores
+biplot(sq_all.pca)
 
 # Varimax Rotated Principal Components
 # retaining 2 components
-fit <- principal(na.omit(selfish_latent_all), nfactors=2, rotate="varimax")
-fit # print results
+sq_all.pca <- principal(na.omit(selfish_latent_all), nfactors=2, rotate="varimax")
+sq_all.pca # print results
 
 # Determine Number of Factors to Extract
 ev <- eigen(cor(na.omit(selfish_latent_all))) # get eigenvalues
@@ -30,3 +36,12 @@ ap <- parallel(subject=nrow(na.omit(selfish_latent_all)),var=ncol(selfish_latent
                rep=100,cent=.05)
 nS <- nScree(x=ev$values, aparallel=ap$eigen$qevpea)
 plotnScree(nS)
+
+
+#exploratory factor analysis
+
+# retaining 3 components
+sq_all.fa3 <- factanal(na.omit(selfish_latent_all), factors=3, rotation="varimax", scores="regression")
+
+# retaining 2 components
+sq_all.fa2 <- factanal(na.omit(selfish_latent_all), factors=2, rotation="varimax", scores="regression")
